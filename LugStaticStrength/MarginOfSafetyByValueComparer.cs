ea228122f69aabf88fc2e3cf404e0205e04742ab
Viktor Sotsuk <sotsukv@gmail.com>
2022-10-14 15:17:24 +0400

    Class MarginOfSafetyByValueComparer is added

---
+++ LugStaticStrength/MarginOfSafetyByValueComparer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LugStaticStrength
+{
+    internal class MarginOfSafetyByValueComparer : IComparer<MarginOfSafety>
+    {
+        public int Compare(MarginOfSafety x, MarginOfSafety y)
+        {
+            return x.Value.CompareTo(x.Value);
+        }
+    }
+}
