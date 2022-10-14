95bbe359294aab2dca7bc8d6921b7131af628dd5
Viktor Sotsuk <sotsukv@gmail.com>
2022-10-14 17:22:00 +0400

    Classes for handling static streingth tests data were moved to folder  Static Tests Data

---
+++ LugStaticStrength/Static Tests Data/DataPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LugStaticStrength
+{
+    public class DataPoint : IEquatable<DataPoint>
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public DataPoint(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"X = {X}; Y = {Y}";
+        }
+
+        public bool Equals(DataPoint other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+    }
+}
--- LugStaticStrength/Static Tests Data/TestDataHandler.cs
+++ LugStaticStrength/Static Tests Data/TestDataHandler.cs
@@ -3,28 +3,6 @@ using System.Collections.Generic;
 
 namespace LugStaticStrength
 {
-    public class DataPoint : IEquatable<DataPoint>
-    {
-        public double X { get; set; }
-        public double Y { get; set; }
-
-        public DataPoint(double x, double y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public override string ToString()
-        {
-            return $"X = {X}; Y = {Y}";
-        }
-
-        public bool Equals(DataPoint other)
-        {
-            return X == other.X && Y == other.Y;
-        }
-    }
-
     public static class TestDataHandler
     {
         public static double GetPolynomialValue(List<double> polynomialСoefficients, double variable)
